c55c8800b2ead035f929e3ddbe0eed3d26c7bfb5
shengweihuang <superhsw@163.com>
2017-04-04 18:19:08 +0800

    attached django2

--- pikachu/wsgi.py
+++ pikachu/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 import sys
-root = os.path.dirname(__file__)                                             #输出当前文件所在文件夹的绝对路径
-sys.path.insert(0, os.path.join(root, '..', 'site-packages'))                #将root路径中的上级目录的site-packages加入到Path中去
+root = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(root, '..', 'site-packages'))               
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pythondjangotest.settings")
 
 
